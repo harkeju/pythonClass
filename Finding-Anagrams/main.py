5f64716651751f1d6d4ca1a277c3b237538ba13f
harkeju <61701308+harkeju@users.noreply.github.com>
2022-05-28 23:49:25 +0100

    Update main.py

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -7,5 +7,19 @@
 def find_anagram(word, anagram):
     # [assignment] Add your code here
 
-    return True
+    word = input("Enter your first word here: ")
+    anagram = input("Enter another word here to check if it is an angram of your first word: ")
 
+    word = word.lower()
+    anagram = anagram.lower()
+
+    if len(word) == len(anagram):
+        sorted_word = sorted(word)
+        sorted_anagram = sorted(anagram)
+
+    if sorted_word == sorted_anagram:
+        return True
+    else:
+        return False
+
+print(find_anagram("word", "anagram"))
